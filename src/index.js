995b00bd035c8ffd98620f059d5d6cb0fe8fa372
Nathan Faucett <nathanfaucett@gmail.com>
2015-05-01 12:17:06 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function capitalizeString(string) {
+module.exports = capitalizeString;
+
+
+function capitalizeString(string) {
     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
-};
+}
